6c0dc3fd6c6c5ed87b83cef72de72dfef25a8fb0
Parv-gugnani <9765.parv@gmail.com>
2024-03-06 11:38:15 +0530

    finally working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import "./App.css";
+import emailjs from "@emailjs/browser";
 
-function App() {
+// z5R67PY6AZ6eSaGI5;
+// //
+// template_bwbit6b;
+
+export default function App() {
+  const emailRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    emailjs.init("z5R67PY6AZ6eSaGI5");
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const serviceId = "service_7qhwn6u";
+    const templateId = "template_bwbit6b";
+    try {
+      setLoading(true);
+      await emailjs.send(serviceId, templateId, {
+        name: nameRef.current?.value,
+        recipient: emailRef.current?.value,
+      });
+      alert("Email successfully sent. Check your inbox.");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <aside></aside>
+      <form className="for" onSubmit={handleSubmit}>
+        <div className="form_group">
+          <label htmlFor="">name</label>
+          <input ref={nameRef} placeholder="Enter your name" />
+        </div>
+        <div className="form_group">
+          <label htmlFor="">email</label>
+          <input ref={emailRef} type="email" placeholder="Enter your email" />
+        </div>
+        <button className="btn" disabled={loading}>
+          Subscribe
+        </button>
+      </form>
+    </section>
   );
 }
-
-export default App;
